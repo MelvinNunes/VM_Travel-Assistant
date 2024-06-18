e763752b9618401cc1cc9bfbed7346810149628e
Melvin Nunes <melvinnunes14@gmail.com>
2024-06-18 16:06:27 +0200

    feat: added translation to the project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
+import './lang/i18n';
+
 import {
   createBrowserRouter,
   RouterProvider,
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,11 @@
+import { useTranslation } from "react-i18next";
+
 export default function Home() {
+    const { t } = useTranslation();
+
     return (
         <div>
-            <h1>Hello World</h1>
+            <h1>{t('hello')}</h1>
         </div>
     )
 }

fd0dbbd3d697cbdb86f9b2d4c735379ab0bcaf79
Melvin Nunes <melvinnunes14@gmail.com>
2024-06-18 10:52:48 +0200

    feat: added shadcn library and dark mode option

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css'
+import { Button } from '@/components/ui/button'
+import { ThemeProvider } from './providers/ThemeProvider'
+import { ModeToggle } from './components/ui/theme-toggler'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+        <Button>Button</Button>
+
+        <ModeToggle />
+      </h1>
+    </ThemeProvider>
   )
 }
 

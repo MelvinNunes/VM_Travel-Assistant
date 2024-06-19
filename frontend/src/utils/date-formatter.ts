473b9e50ec66df9b590041732aafc23ea2891bbe
Melvin Nunes <melvinnunes14@gmail.com>
2024-06-20 00:48:23 +0200

    fix: just a lil tweak

--- frontend/src/utils/date-formatter.ts
+++ frontend/src/utils/date-formatter.ts
@@ -4,7 +4,7 @@ import { ptBR } from "date-fns/locale";
 export class DateFormatter {
   static formatDate(dateString: string): string {
     const date = new Date(dateString);
-    const formattedDate = format(date, "d 'de' MMMM 'de' yyyy", {
+    const formattedDate = format(date, "d 'de' MMMM 'de' yyyy 'às' H:mm", {
       locale: ptBR,
     });
     return formattedDate;
